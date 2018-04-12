37ffcdfca66a11365ff88aeed5db9f38b97ba2c6
Adam Haile <adam.haile@gmail.com>
2018-04-12 11:55:49 -0400

    router: read in initial state before flushing our state to hash (#1)

--- src/router.ts
+++ src/router.ts
@@ -3,16 +3,6 @@ import { ToDosCtrl } from './controllers';
 
 // with such a simple router scenario, no need for a lib, just hand-write it
 export function ToDosRouter(ctrl : ToDosCtrl) {
-    // filter() -> browser hash
-    S(() => {
-        var filter = ctrl.filter(),
-            hash   = filter === true  ? "/completed" :
-                     filter === false ? "/active"    :
-                     "/";
-
-        if (window.location.hash !== hash) window.location.hash = hash;
-    });
-
     // browser hash -> filter()
     window.addEventListener('hashchange', setStateFromHash, false);
     S.cleanup(function () { window.removeEventListener('hashchange', setStateFromHash); });
@@ -22,9 +12,19 @@ export function ToDosRouter(ctrl : ToDosCtrl) {
                      hash === "#/active"    ? false :
                      null;
 
-        if (ctrl.filter() !== filter) ctrl.filter(filter);
+        ctrl.filter(filter);
     }
 
     // init from browser hash
-    S.sample(setStateFromHash);
+    setStateFromHash();
+
+    // filter() -> browser hash
+    S(() => {
+        var filter = ctrl.filter(),
+            hash   = filter === true  ? "/completed" :
+                     filter === false ? "/active"    :
+                     "/";
+
+        window.location.hash = hash;
+    });
 }
